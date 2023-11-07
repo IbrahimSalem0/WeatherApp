5432aff78ed44162d723ed7c8dc28e7ad8f8091a
Ibrahim Salem <i.salem@soum.sa>
2023-11-07 22:36:55 +0200

    feat:  add API config

---
+++ src/Apis/axiosConfig.ts
@@ -0,0 +1,32 @@
+// axiosConfig.ts
+
+import axios from 'axios';
+
+const BASE_URL = 'http://api.weatherapi.com/v1/current.json';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
